7c0ca6d5372122d28e937e3de5f6bfe8264aa80d
tejasmaroo <143869588+tejasmaroo@users.noreply.github.com>
2023-09-06 19:20:12 +0530

    -

---
+++ art.py
@@ -0,0 +1,24 @@
+logo = """
+                                                                                                             
+88888888ba                                                                                           88      
+88      "8b                                                                                          88      
+88      ,8P                                                                                          88      
+88aaaaaa8P'  ,adPPYYba,  ,adPPYba,  ,adPPYba,  8b      db      d8   ,adPPYba,   8b,dPPYba,   ,adPPYb,88      
+88""""""'    ""     `Y8  I8[    ""  I8[    ""  `8b    d88b    d8'  a8"     "8a  88P'   "Y8  a8"    `Y88      
+88           ,adPPPPP88   `"Y8ba,    `"Y8ba,    `8b  d8'`8b  d8'   8b       d8  88          8b       88      
+88           88,    ,88  aa    ]8I  aa    ]8I    `8bd8'  `8bd8'    "8a,   ,a8"  88          "8a,   ,d88      
+88           `"8bbdP"Y8  `"YbbdP"'  `"YbbdP"'      YP      YP       `"YbbdP"'   88           `"8bbdP"Y8      
+                                                                                                                                                                                                                        
+  ,ad8888ba,                                                                                                  
+ d8"'    `"8b                                                                 ,d                              
+d8'                                                                           88                              
+88              ,adPPYba,  8b,dPPYba,    ,adPPYba,  8b,dPPYba,  ,adPPYYba,  MM88MMM   ,adPPYba,   8b,dPPYba,  
+88      88888  a8P_____88  88P'   `"8a  a8P_____88  88P'   "Y8  ""     `Y8    88     a8"     "8a  88P'   "Y8  
+Y8,        88  8PP"""""""  88       88  8PP"""""""  88          ,adPPPPP88    88     8b       d8  88          
+ Y8a.    .a88  "8b,   ,aa  88       88  "8b,   ,aa  88          88,    ,88    88,    "8a,   ,a8"  88          
+  `"Y88888P"    `"Ybbd8"'  88       88   `"Ybbd8"'  88          `"8bbdP"Y8    "Y888   `"YbbdP"'   88          
+                                                                                                              
+                                                                                                              
+
+
+"""
--- pypasswordgenerator.py
+++ pypasswordgenerator.py
@@ -1,5 +1,7 @@
 import random
+from art import logo
 
+print(logo)
 print("\nWelcome to the PyPassword Genertor!\n")
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
